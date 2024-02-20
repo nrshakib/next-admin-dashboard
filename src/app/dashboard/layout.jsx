bb29c5b685e3d134630f58186c66bd3532f78691
NRS\nrsha <nrshakib007@gmail.com>
2024-02-21 01:18:05 +0600

    Added Dashboard Pages and Components

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,5 +1,18 @@
-const Layout = () => {
-  return <div></div>;
+import Navbar from "@/components/dashboard/navbar/Navbar";
+import Sidebar from "@/components/dashboard/sidebar/Sidebar";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
