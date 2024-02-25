5b41daef32e47bef2d3f1a07713a7947aea79cb0
NRS\nrsha <nrshakib007@gmail.com>
2024-02-25 12:33:16 +0600

    Added Navbar Design

--- src/components/dashboard/navbar/Navbar.jsx
+++ src/components/dashboard/navbar/Navbar.jsx
@@ -9,13 +9,19 @@ const Navbar = () => {
   const pathName = usePathname();
   return (
     <div className="flex justify-between items-center p-5 rounded-lg bg-gray-800">
-      <div className="text-gray-400 font-bold capitalize">{pathName.split("/").pop()}</div>
-      <div className="flex items-center">
-        <div className="flex">
+      <div className="text-gray-400 font-bold capitalize">
+        {pathName.split("/").pop()}
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 bg-gray-600 p-2 rounded-lg ">
           <FaSearch />
-          <input type="text" placeholder="Search.." className="" />
+          <input
+            type="text"
+            placeholder="Search.."
+            className="bg-transparent border-none outline-none text-gray-300"
+          />
         </div>
-        <div className="flex">
+        <div className="flex gap-2">
           <MdMessage size={20} />
           <MdOutlineNotifications size={20} />
           <BiWorld size={20} />
