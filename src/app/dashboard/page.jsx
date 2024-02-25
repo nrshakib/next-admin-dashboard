1b5b7f948b04777d24397701692ad5e15c900f89
NRS\nrsha <nrshakib007@gmail.com>
2024-02-25 15:58:09 +0600

    Added Components to Dashboard Page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,5 +1,25 @@
+import Chart from "@/components/dashboard/chart/Chart";
+import RightBar from "@/components/dashboard/rightbar/RightBar";
+import Summary from "@/components/dashboard/summary/Summary";
+import Transactions from "@/components/dashboard/transactions/Transactions";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <Summary />
+          <Summary />
+          <Summary />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div>
+        <RightBar />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
